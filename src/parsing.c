2f9856aed5d3602429bfadaf8e6ca5a13163e4c2
jstar1 <jusmoore@pdx.edu>
2016-06-20 09:25:12 -0700

    Ability to use operators to perform operations

--- src/parsing.c
+++ src/parsing.c
@@ -5,8 +5,51 @@
 #include <editline/history.h>
 #include "mpc.h" //link to the mpc library  NOTE i am using "" because i am searching the local directory for this header file
 
+
+/* Defining eval_op, test for which operator is passed in and performs the C operation on input */
+long eval_op(long x, char *op, long y){
+	
+	if(strcmp(op, "+") == 0) {return x + y;}
+
+	if(strcmp(op,"-") == 0) {return x - y;}
+
+	if(strcmp(op, "*") == 0) {return x * y;}
+
+	if(strcmp(op, "/") == 0) {return x / y;}
+
+	return 0;
+}
+
+/*recurrsive evaluation function */
+long eval(mpc_ast_t *t){
+	
+	/*If tagged as number return it directly. */
+	if(strstr(t -> tag, "number"))
+	{
+		return atoi(t -> contents);
+	}
+
+	/*The operator is always second child*/
+	char *op = t -> children[1] -> contents;
+
+	/* We store the third child in 'x' */
+	long x = eval(t -> children[2]);
+
+	/*Iterate the remaining children and comvining */
+	int i = 3;
+
+	while(strstr(t -> children[i] -> tag, "expr"))
+	{
+		x = eval_op(x, op, eval(t -> children[i]));
+		++i;
+	}
+
+	return x;
+}	
+
 int main(int argc, char ** argv) 
 {
+
 	/*Creating parsers */
 
 	mpc_parser_t *Number 	= mpc_new("number");
@@ -47,28 +90,27 @@ int main(int argc, char ** argv)
 		/* Add input to history */
 		add_history(input);
 
+		/*This section of code displays the tree*/
 		/*Attempt to Parse the user Input */
 		mpc_result_t r;
 		
+
+				
 		if(mpc_parse("<stdin>", input, Lispy, &r))
 		{
-			/*If this is succesful*/
-			mpc_ast_print(r.output);
+			mpc_ast_t *result = r.output;
+			printf("%li\n",eval(result));
 			mpc_ast_delete(r.output);
 		} 
 		
 		else
 		{
-			/*If there is an error */
+			//If there is an error 
 
 			mpc_err_print(r.error);
 			mpc_err_delete(r.error);
 		}
-		
-		/*
-		 Echo input back to user 
-		printf("Is this what you typed %s\n", input);
-		*/
+			
 		
 		/*Free memory created for input*/
 		free(input);
